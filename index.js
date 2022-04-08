abde898f455a903b755084c3fa995d265e618125
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-04-07 21:05:45 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+//used const to create totalBatteries variable
+//batteryBatches array already created above, so...sum means total
+//use reduce()with batteryBatches, create a function to use arguments for result
+//add batches and total with return 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+    }
+  )
